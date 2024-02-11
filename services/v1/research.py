4e5963a1cf16448c364669a88bf023b5d35a7293
zetempesta <zetempesta@gmail.com>
2024-02-11 00:40:37 -0400

    Insert Research

--- services/v1/research.py
+++ services/v1/research.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
-from controller.research import list_research
+from controller.research import insert_research, list_research
 from model.research import Research 
 from core.parameters import PAGE_SIZE
 
 router = APIRouter()
 
-@router.get("/researchs", response_model=list[Research])
-async def get_researchs():
+@router.get("/research", response_model=list[Research])
+async def get_research():
     return list_research()
+
+@router.post("/research")
+async def post_research(research:Research)->bool:
+    return insert_research(research)
